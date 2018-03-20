546ad4dbbf81141944ebbce181947f4a3a67794a
Vlad Burian <vovaburyan@gmail.com>
2018-03-19 21:28:08 -0700

    Fork changes

--- app/src/androidTest/java/com/tubitv/tubitv/Configuration.kt
+++ app/src/androidTest/java/com/tubitv/tubitv/Configuration.kt
@@ -4,5 +4,5 @@ package com.tubitv.tubitv
  * Created by vburian on 2/19/18.
  */
 val appPackage="com.tubitv"
-val globalTimeout=7000L
-val moviesListTimeout = 9000L
+val globalTimeout=10000L
+val moviesListTimeout = 10000L
